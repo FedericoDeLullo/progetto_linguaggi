45d79b9dbd5974d7c2560dc3274cb92a4cea2018
FedericoDeLullo <131187380+FedericoDeLullo@users.noreply.github.com>
2023-07-25 17:32:49 +0200

    Add files via upload

---
+++ progetto/file_php/acquisto.php
@@ -0,0 +1,19 @@
+<?php
+require_once("connection.php");
+session_start();
+
+if (isset($_POST['conferma'])) {
+  if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+  }
+  if (empty($_SESSION['carrello'])) {
+    header("Location: ../HTML/acquisto_ko.html");
+    exit();}
+
+  header("Location: ../HTML/acquisto_ok.html");
+  exit();
+} else {
+  header("Location: ../HTML/acquisto_ko.html");
+}
+?>
